152c8a4143ca6044433243834fd2509f1c3bf0fd
Lavric Cosmin-Marian <cosmin.lavric@student.unitbv.ro>
2024-02-22 22:10:34 +0200

    First Commit

---
+++ dice.js
@@ -0,0 +1,20 @@
+  // Player1
+    var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+    var randomImageSource = "images/dice" + randomNumber1 + ".png";
+
+    var image1 = document.querySelectorAll("img")[0];
+    image1.setAttribute("src", randomImageSource);
+
+    // Player2
+    var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+    var image2 = document.querySelectorAll("img")[1];
+    image2.setAttribute("src", randomImageSource2);
+    if (randomNumber1 > randomNumber2) {
+      document.querySelector("h1").innerHTML = "🚩 Player1 wins!";
+    } else if (randomNumber2 > randomNumber1) {
+      document.querySelector("h1").innerHTML = "🚩Player2 wins!";
+    } else {
+      document.querySelector("h1").innerHTML = "🏳️ It's a draw!";
+    }
